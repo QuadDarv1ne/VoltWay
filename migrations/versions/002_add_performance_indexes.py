d10d5c4b9b57fb77cf45a8f970162be967bc564c
QuadDarv1ne <maksimqwe42@mail.ru>
2026-02-22 13:54:45 +0300

    modified:   migrations/versions/002_add_performance_indexes.py

--- migrations/versions/002_add_performance_indexes.py
+++ migrations/versions/002_add_performance_indexes.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '002'
-down_revision = '001_initial_migrate'
+down_revision = '001_initial'
 branch_labels = None
 depends_on = None
 
